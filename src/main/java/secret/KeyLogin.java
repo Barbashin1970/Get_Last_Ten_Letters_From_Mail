5d3899de2a64e84e0aefd48aa085202ba711d64c
Oleg <110758640+Barbashin1970@users.noreply.github.com>
2023-07-25 15:18:42 +0700

    Update KeyLogin.java

--- src/main/java/secret/KeyLogin.java
+++ src/main/java/secret/KeyLogin.java
@@ -1,12 +1,12 @@
 package secret;
 
 public class KeyLogin {
-    public static String emailYandex = "tipayTest2@yandex.ru";
-    //пароль от тестового почтового ящика на yandex  2023tipayTest123456
-    public static String emailForMailRu = "tipaytest@mail.ru";
-    //пароль от тестового почтового ящика на mail.ru  2023testTipay123456
+    public static String emailYandex = "t@yandex.ru";
+
+    public static String emailForMailRu = "t@mail.ru";
+ 
 
     //это пароли для приложений, который устанавливается из почтового ящика
-    public static String passwordYandex = "ihohoejoaovovyjx";
-    public static String passwordMail = "E31wnPQhQ9AXWLQVJZrt";
+    public static String passwordYandex = "ih";
+    public static String passwordMail = "E";
 }
