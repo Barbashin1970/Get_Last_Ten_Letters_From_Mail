9c9bb13d691079658919e757eae2a7df491771b2
Oleg_Barbashin <banksnab@gmail.com>
2023-07-25 15:11:38 +0700

    commit 2

---
+++ src/main/java/secret/Credenrials.java
@@ -0,0 +1,12 @@
+package secret;
+
+public class Credenrials {
+    public static String emailYandex = "tipayTest2@yandex.ru";
+    //пароль от тестового почтового ящика на yandex  2023tipayTest123456
+    public static String emailForMailRu = "tipaytest@mail.ru";
+    //пароль от тестового почтового ящика на mail.ru  2023testTipay123456
+
+    //это пароли для приложений, который устанавливается из почтового ящика
+    public static String passwordYandex = "ihohoejoaovovyjx";
+    public static String passwordMail = "E31wnPQhQ9AXWLQVJZrt";
+}
